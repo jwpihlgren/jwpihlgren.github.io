c9dda806b8df442024a3caf553d4f6fc33564d40
Joachim Pihlgren <j.w.pihlgren@gmail.com>
2020-10-05 23:14:35 +0200

    Added untracked files

---
+++ test.js
@@ -0,0 +1,28 @@
+const meny = {
+    vecka: "39",
+    måndag: {
+        fisk : "Lax soppa",
+        kött: "Lax soppa",
+        veg:"Lax soppa",
+    },
+    måndag: {
+        fisk : "Lax soppa",
+        kött: "Lax soppa",
+        veg:"Lax soppa",
+    },
+    måndag: {
+        fisk : "Lax soppa",
+        kött: "Lax soppa",
+        veg:"Lax soppa",
+    },
+    måndag: {
+        fisk : "Lax soppa",
+        kött: "Lax soppa",
+        veg:"Lax soppa",
+    },
+    måndag: {
+        fisk : "Lax soppa",
+        kött: "Lax soppa",
+        veg:"Lax soppa",
+    },
+}
